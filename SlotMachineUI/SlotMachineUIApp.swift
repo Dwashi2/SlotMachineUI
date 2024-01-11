0b68156b111b4c0981fdec843d4e8649fd1f7e79
Daniel Washington Ignacio <danielvertigo@hotmail.com>
2024-01-11 13:47:33 -0300

    Initial Commit

---
+++ SlotMachineUI/SlotMachineUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotMachineUIApp.swift
+//  SlotMachineUI
+//
+//  Created by Daniel Washington Ignacio on 11/01/24.
+//
+
+import SwiftUI
+
+@main
+struct SlotMachineUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
